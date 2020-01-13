309655c4be4598c0b39258ecd2ff12db5e453543
Adam Czarnecki <aczarnecki93@gmail.com>
2020-01-13 23:42:50 +0100

    some major repairs

--- game.js
+++ game.js
@@ -1,87 +1,130 @@
-const position = {
-    bear: 2,
-    bone: 0
+const state = {
+    boardHeight: 10,
+    boardWidth: 10,
+    bearX: 0,
+    bearY: 0,
+    boneX: 0,
+    boneY: 0,
+    bearDirection: "right",
+    score: 0,
+    c: 0
 }
 
-//Flag
+// let c;
 
-let bearDirection = "right";
+// const bear = {
+//     x: 0,
+//     y: 0
+// }
+
+
+
+const board = [...document.querySelectorAll(".board div")];
+
+
 
 // Funkcja losująca diva z bonem
 
 const randomBone = () => {
-    const divNumber = document.querySelector(".board").childElementCount;
+    state.boneX = Math.floor(10 * Math.random())
+    state.boneY = Math.floor(10 * Math.random())
 
-    const randomBoneDiv = Math.floor(Math.random() * (divNumber + 1));
-
-    document.querySelector(`div:nth-child(${randomBoneDiv})`).classList.add("bone");
-    position.bone = randomBoneDiv;
 };
+
+// ustalanie pozycji bona i beara
+
+const position = (a, b) => {
+    return a + b * state.boardHeight
+}
+
+// renderowanie  bone i beara
+
+const render = function () {
+    for (let a = 0; a < board.length; a++) {
+        board[a].classList.remove("bear")
+    };
+    let b = position(state.bearX, state.bearY)
+    board[b].classList.add("bear");
+    state.c = position(state.boneX, state.boneY)
+    board[state.c].classList.add("bone");
+
+};
+
+
+// Funkcja GAME OVER - nieskończona 
+
+const gameOver = () => {
+    if (state.bearX < 0 || state.bearX > 9) {
+        clearInterval(timer);
+        document.querySelector("section").remove();
+        document.querySelector(".end").style.display = "block"
+    }
+}
+
+// pojedynczy koniec gry ze względu na oś Y
+
+const gameEnd = () => {
+    clearInterval(timer);
+    document.querySelector("section").remove();
+    document.querySelector(".end").style.display = "block"
+}
 
 // Funkcja odpowiedzialna za kierunek beara
 
 const changeBearDirection = (e) => {
 
-    if (e.keyCode == 37) {
-        bearDirection = "left"
+    if (e.keyCode === 37) {
+        state.bearDirection = "left"
     } else if (e.keyCode === 39) {
-        bearDirection = "right"
+        state.bearDirection = "right"
     } else if (e.keyCode === 38) {
-        bearDirection = "up"
+        state.bearDirection = "up"
     } else if (e.keyCode === 40) {
-        bearDirection = "down"
+        state.bearDirection = "down"
     };
+
 };
 
-// Funkcja GAME OVER - nieskończona 
+// Funkcja odpowiedzialna za zmianę kierunku beara oraz ponowne jego wyrenderowanie
 
-const loss = () => {
-    if (position.bear <= 0 || position.bear >= 101) {
-        document.querySelector(".board").remove()
-        document.querySelector(".end").style.display = "block"
-        clearInterval(interval)
-    };
-};
-
-// Funkcja odpowiedzialna za ruch beara
-
-const bearMove = () => {
-    loss()
-    document.querySelectorAll("div").forEach(div => {
-        div.classList.remove("bear")
-    });
-    if (bearDirection === "right") {
-        document.querySelector(`div:nth-child(${++position.bear})`).classList.add("bear");
-    } else if (bearDirection === "left") {
-        document.querySelector(`div:nth-child(${--position.bear})`).classList.add("bear");
-
-    } else if (bearDirection === "up") {
-        document.querySelector(`div:nth-child(${position.bear -= 10})`).classList.add("bear");
-    } else if (bearDirection === "down") {
-        document.querySelector(`div:nth-child(${position.bear += 10})`).classList.add("bear");
+const bearNewDirection = () => {
+    if (state.bearDirection === "left") {
+        state.bearX--
+    } else if (state.bearDirection === "right") {
+        state.bearX++
+    } else if (state.bearDirection === "up") {
+        state.bearY--
+    } else if (state.bearDirection === "down") {
+        state.bearY++
     }
-    getScore();
+    for (let a = 0; a < board.length; a++) {
+        board[a].classList.remove("bear")
+    };
+    let b = position(state.bearX, state.bearY)
+    if (state.bearY < 0 || state.bearY > 9) {
+        gameEnd()
+    }
+    board[b].classList.add("bear");
+
+    if (b === state.c) {
+        state.score++
+        for (let a = 0; a < board.length; a++) {
+            board[a].classList.remove("bone")
+        };
+        randomBone();
+        state.c = position(state.boneX, state.boneY)
+        board[state.c].classList.add("bone");
+        document.querySelector(".score").textContent = state.score
+    }
+    gameOver()
 }
 
-// Funkcja odpowiedzialna za wyniki
-
-const getScore = () => {
-    if (position.bear === position.bone) {
-        document.querySelectorAll("div").forEach(div => {
-            div.classList.remove("bone");
-        });
-        randomBone();
-        document.querySelector(".result span").textContent++;
-    };
-};
-
-// Funkcja startująca grę
 
 const startGame = () => {
-    document.querySelector(`div:nth-child(${position.bear})`).classList.add("bear")
-    randomBone()
-};
-const interval = setInterval(bearMove, 150)
+    randomBone();
+    render()
+}
+const timer = setInterval(bearNewDirection, 200)
 
 
 window.setTimeout(startGame, 200);
